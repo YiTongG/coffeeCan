30b2af06f5247abc34bf678b939f85d3a6643c31
yitong gong <gongyitong0201@gmail.com>
2024-07-13 13:52:56 -0400

    userdata

---
+++ api/routes/post.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+//import {verifyToken} from "../middleware/verifyToken.js";
+//import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+
+//router.post("/", addPost);
+
+
+export default router;
