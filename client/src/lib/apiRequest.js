513301ed42771588b8eb8d807964f5e9aec60536
yitong gong <gongyitong0201@gmail.com>
2024-08-24 18:01:50 -0400

    replace api to aws with https connection

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //base url
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,  // allowed cookie
 });
 export default apiRequest;
