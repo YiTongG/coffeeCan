4c2ae519f8b8cf8497af9ed40f2b62207c7220bf
yitong gong <gongyitong0201@gmail.com>
2024-08-24 14:44:29 -0400

    Cross-Origin Resource Sharing for aws

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 //base url
 const apiRequest = axios.create({
-  
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,  //using cookie
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  withCredentials: true,  // allowed cookie
 });
-
 export default apiRequest;
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:8800',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
